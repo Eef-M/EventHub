c32290c6b4e564c8442961283a66c2814cddf738
Eef-M <eefmekel@gmail.com>
2025-04-30 18:44:47 +0700

    feat!: add Login page, Event card components and add router view on App.vue

---
+++ frontend/src/components/EventCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-white rounded-2xl shadow p-5 hover:shadow-lg transition">
+    <img class="w-full h-48 object-cover rounded-lg mb-4"
+      src="https://images.pexels.com/photos/3861969/pexels-photo-3861969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      alt="Event Thumbnail" />
+
+    <h2 class="text-xl font-semibold text-gray-800 mb-2">
+      Event Title
+    </h2>
+
+    <p class="text-sm text-gray-600 mb-1">
+      📍 Location Name
+    </p>
+
+    <p class="text-sm text-gray-600 mb-3">
+      📅 April 30, 2025
+    </p>
+
+    <p class="text-gray-700 text-sm">
+      This is a short description of the event. It provides a quick overview of what to expect.
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ frontend/src/pages/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="bg-white shadow-md rounded-lg p-8 w-full max-w-md">
+      <h2 class="text-3xl font-semibold text-center text-indigo-800 mb-6">Login</h2>
+
+      <form>
+        <div class="mb-4">
+          <label class="block text-gray-700 mb-2" for="email">Email</label>
+          <input type="email" id="email"
+            class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            placeholder="email@example.com" />
+        </div>
+
+        <div class="mb-6">
+          <label class="block text-gray-700 mb-2" for="password">Password</label>
+          <input type="password" id="password"
+            class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            placeholder="********" />
+        </div>
+
+        <button type="submit" class="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-blue-700 transition">
+          Login
+        </button>
+      </form>
+
+      <p class="mt-6 text-sm text-center text-gray-600">
+        Don't have an account yet?
+        <RouterLink to="/register" class="text-indigo-600 hover:underline">Register</RouterLink>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
