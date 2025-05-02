b1d7930ab7e084c22f3d86ec65a8b7e871fd53c7
Eef-M <eefmekel@gmail.com>
2025-05-02 21:03:28 +0700

    feat!: add events page and router link

--- frontend/src/pages/Events.vue
+++ frontend/src/pages/Events.vue
@@ -1,20 +1,5 @@
 <template>
   <DefaultLayout>
-    <section class="py-20">
-      <div class="max-w-7xl mx-auto px-4 text-center">
-        <h1 class="text-5xl font-bold text-gray-900 mb-4">Discover Exciting Events</h1>
-        <p class="text-lg text-gray-600 mb-8">
-          Explore and manage event easily.
-        </p>
-        <div class="flex justify-center gap-4">
-          <RouterLink to="/events"
-            class="px-6 py-3 bg-blue-600 text-white rounded-xl shadow hover:bg-blue-700 transition">
-            Browse Events
-          </RouterLink>
-        </div>
-      </div>
-    </section>
-
     <section class="bg-white py-16">
       <div class="max-w-7xl mx-auto px-4">
         <h2 class="text-3xl font-semibold text-center mb-10">News Events</h2>
@@ -34,8 +19,9 @@
   </DefaultLayout>
 </template>
 
-<script setup lang="ts">
-import EventCard from '../components/EventCard.vue';
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router';
 import DefaultLayout from '../layouts/DefaultLayout.vue';
+import EventCard from '../components/EventCard.vue';
 
 </script>
